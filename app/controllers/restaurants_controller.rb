ab7b7723d3ece1735431a85058ee9ac59b635618
Constantin Pahl <constantin.pahl@gmail.com>
2017-07-12 16:22:00 +0200

    controller restaurants & reviews updated with methods

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,16 +1,38 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show]
   def index
-  end
-
-  def create
+    @restaurants = Restaurant.all
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
-  def edit
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    respond_to do |format|
+      if @restaurant.save
+        format.html { redirect_to @restaurant, notice: 'Restaurant was successfully created.' }
+        format.json { render :show, status: :created, location: @restaurant }
+      else
+        format.html { render :new }
+        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def show
   end
+
+private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
-
-  get 'reviews/create'
-
-  get 'reviews/new'
-
-  get 'restaurants/index'
-
-  get 'restaurants/create'
-
-  get 'restaurants/new'
-
-  get 'restaurants/show'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
 end

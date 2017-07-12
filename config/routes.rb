b3233ebbb22abcaf0a39ec275313310e67d8a327
Constantin Pahl <constantin.pahl@gmail.com>
2017-07-12 14:44:19 +0200

    controller Restaurants and Reviews added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+
+  get 'reviews/create'
+
+  get 'reviews/new'
+
+  get 'restaurants/index'
+
+  get 'restaurants/create'
+
+  get 'restaurants/new'
+
+  get 'restaurants/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number:  "04 050 0660 503",
+    category:     "chinese"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number:  "04 050 0660 503",
+    category:     "italian"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number:  "04 050 0660 503",
+    category:     "french"
+  },
+  {
+    name:         "A la bière fraiche",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number:  "04 050 0660 503",
+    category:     "belgian"
+  },
+  {
+    name:         "Ramen Ya",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number:  "04 050 0660 503",
+    category:     "japanese"
+  }
+]
+
